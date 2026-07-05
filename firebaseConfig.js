f29f4727da6e4d0f2ea88b248d2a2719639e4bf5
Mason <masonwicik1@gmail.com>
2026-07-05 19:43:54 -0400

    updated

--- firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAi7d-DvIKT-WMnCiCksUrfx1Zaw1pmXX8",
-    authDomain: "catan-game-tracker.firebaseapp.com",
-    projectId: "catan-game-tracker",
-    storageBucket: "catan-game-tracker.firebasestorage.app",
-    messagingSenderId: "388660545030",
-    appId: "1:388660545030:web:ba397953d8fa538943a587"
-};
